72640f558a184945bbef89259d10d3038296b542
javid.shoaei <javid.shoaei@brainamics.de>
2023-04-25 10:45:33 +0200

    Add mobile game loader base

---
+++ Runtime/Core/MobileGameLoaderBase.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public abstract class MobileGameLoaderBase : MonoBehaviour
+    {
+        public bool IsPaused { get; private set; }
+
+        protected abstract void Initialize();
+
+        protected virtual void PauseStateChanged(bool paused)
+        {
+        }
+
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+            Initialize();
+        }
+
+        private void OnApplicationPause(bool pause)
+        {
+            SetPause(pause);
+        }
+
+        private void OnApplicationFocus(bool focus)
+        {
+            SetPause(!focus);
+        }
+
+        private void SetPause(bool paused)
+        {
+            if (IsPaused == paused)
+                return;
+            PauseStateChanged(paused);
+        }
+    }
+}
