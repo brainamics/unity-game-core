4babe6c2e5c21a54824d8d8e6350828799705fe8
javid.shoaei <javid.shoaei@brainamics.de>
2023-05-22 10:53:00 +0200

    Add TimeScaleManager

---
+++ Runtime/Timing/TimeScaleManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public class TimeScaleManager : MonoBehaviour
+    {
+        private bool _paused;
+        private float _normalTimescale = 1f;
+
+        public bool IsPaused
+        {
+            get => _paused;
+            set
+            {
+                if (_paused == value) return;
+                _paused = value;
+                UpdateTimescale();
+            }
+        }
+
+        public void AddMultiplier(float timescaleMultiplier)
+        {
+            _normalTimescale *= timescaleMultiplier;
+            UpdateTimescale();
+        }
+
+        public void RemoveMultiplier(float timescaleMultiplier)
+        {
+            _normalTimescale /= timescaleMultiplier;
+            UpdateTimescale();
+        }
+
+        private void UpdateTimescale()
+        {
+            Time.timeScale = _paused ? 0f : _normalTimescale;
+        }
+
+        private void OnDestroy()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
