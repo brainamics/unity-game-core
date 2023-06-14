9ad3a8622ccb09db9b2a32e7fd8f7293e9d921cf
javid.shoaei <javid.shoaei@brainamics.de>
2023-06-14 17:24:54 +0200

    Update audio service

---
+++ Runtime/Audio/AudioSourceReference.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public class AudioSourceReference : MonoBehaviour
+    {
+        public AudioSource AudioSource;
+    }
+}
