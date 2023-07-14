17f738d32d2b514490d057bd2b6a4a63dc5a9388
javid.shoaei <javid.shoaei@brainamics.de>
2023-07-14 16:20:26 +0200

    update scene persistence manager

--- Runtime/Persistence/ScenePersistenceManagerBase.cs
+++ Runtime/Persistence/ScenePersistenceManagerBase.cs
@@ -10,7 +10,7 @@ namespace Brainamics.Core
     {
         private readonly List<IPersistentState<TState>> _persistableObjects = new();
         private IPersistenceService<TState> _persistenceService;
-        
+
         [SerializeField]
         private bool _loadOnFirstFrame;
 
@@ -18,11 +18,8 @@ namespace Brainamics.Core
 
         protected abstract IPersistenceService<TState> GetPersistenceService();
 
-        private void Awake()
+        public void RescanSceneObjects()
         {
-            _persistenceService = GetPersistenceService();
-            _persistenceService.SetActiveScenePersistenceManager(this);
-
             _persistableObjects.Clear();
             var scene = gameObject.scene;
             var rootObjects = scene.GetRootGameObjects();
@@ -31,6 +28,14 @@ namespace Brainamics.Core
                 var components = obj.GetComponentsInChildren<IPersistentState<TState>>();
                 _persistableObjects.AddRange(components);
             }
+        }
+
+        private void Awake()
+        {
+            _persistenceService = GetPersistenceService();
+            _persistenceService.SetActiveScenePersistenceManager(this);
+
+            RescanSceneObjects();
         }
 
         private void Start()
