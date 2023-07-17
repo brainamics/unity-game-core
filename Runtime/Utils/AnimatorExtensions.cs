0f0fcf72e2a0579fd171601e0145a622c482dab0
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-07-17 12:28:50 +0200

    Update AnimatorExtensions.cs

--- Runtime/Utils/AnimatorExtensions.cs
+++ Runtime/Utils/AnimatorExtensions.cs
@@ -19,6 +19,25 @@ public static class AnimatorExtensions
         }
     }
 
+    public static IEnumerable WaitForStateDuration(Animator animator, string stateName, int layerIndex = 0)
+    {
+        var waitTime = 0f;
+
+        while (true)
+        {
+            var state = animator.GetCurrentAnimatorStateInfo(layerIndex);
+            if (state.IsName(stateName))
+            {
+                waitTime = state.normalizedTime;
+                break;
+            }
+            yield return null;
+        }
+
+        if (waitTime > 0)
+            yield return new WaitForSeconds(waitTime);
+    }
+
     public static IEnumerable WaitForStateEnd(this Animator animator, string stateName, int layerIndex = 0)
     {
         yield return null;
