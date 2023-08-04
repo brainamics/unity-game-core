34bbcce1fd0b7523b68ab16cbd1aa361a144ccef
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-08-04 13:25:33 +0200

    Add files via upload

---
+++ Runtime/Utils/GameObjectUtils.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameObjectUtils
+{
+    public static GameObject GetGameObject(object o)
+    {
+        if (o == null)
+            return null;
+        return o switch
+        {
+            Transform t => t.gameObject,
+            GameObject obj => obj,
+            Component c => c.gameObject,
+            _ => null,
+        };
+    }
+
+    public static GameObject GetGameObjectOrThrow(object o)
+    {
+        if (o == null)
+            return null;
+        var go = GetGameObject(o);
+        if (go == null)
+            throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
+        return go;
+    }
+
+    public static Transform GetTransform(object o)
+    {
+        var go = GetGameObject(o);
+        return go == null ? null : go.transform;
+    }
+
+    public static Transform GetTransformOrThrow(object o)
+    {
+        if (o == null)
+            return null;
+        var transform = GetTransform(o);
+        if (transform == null)
+            throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
+        return transform;
+    }
+}
