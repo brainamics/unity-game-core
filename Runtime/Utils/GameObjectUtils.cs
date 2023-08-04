a9cc560773bf5af7eb593e4f810f4e92a29ddefd
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-08-04 13:26:06 +0200

    Update GameObjectUtils.cs

--- Runtime/Utils/GameObjectUtils.cs
+++ Runtime/Utils/GameObjectUtils.cs
@@ -2,44 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class GameObjectUtils
+namespace Brainamics.Core
 {
-    public static GameObject GetGameObject(object o)
+    public static class GameObjectUtils
     {
-        if (o == null)
-            return null;
-        return o switch
+        public static GameObject GetGameObject(object o)
         {
-            Transform t => t.gameObject,
-            GameObject obj => obj,
-            Component c => c.gameObject,
-            _ => null,
-        };
-    }
-
-    public static GameObject GetGameObjectOrThrow(object o)
-    {
-        if (o == null)
-            return null;
-        var go = GetGameObject(o);
-        if (go == null)
-            throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
-        return go;
-    }
-
-    public static Transform GetTransform(object o)
-    {
-        var go = GetGameObject(o);
-        return go == null ? null : go.transform;
-    }
-
-    public static Transform GetTransformOrThrow(object o)
-    {
-        if (o == null)
-            return null;
-        var transform = GetTransform(o);
-        if (transform == null)
-            throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
-        return transform;
+            if (o == null)
+                return null;
+            return o switch
+            {
+                Transform t => t.gameObject,
+                GameObject obj => obj,
+                Component c => c.gameObject,
+                _ => null,
+            };
+        }
+    
+        public static GameObject GetGameObjectOrThrow(object o)
+        {
+            if (o == null)
+                return null;
+            var go = GetGameObject(o);
+            if (go == null)
+                throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
+            return go;
+        }
+    
+        public static Transform GetTransform(object o)
+        {
+            var go = GetGameObject(o);
+            return go == null ? null : go.transform;
+        }
+    
+        public static Transform GetTransformOrThrow(object o)
+        {
+            if (o == null)
+                return null;
+            var transform = GetTransform(o);
+            if (transform == null)
+                throw new System.InvalidOperationException($"Cannot resolve GameObject from an object of type '{o.GetType()}'.");
+            return transform;
+        }
     }
 }
