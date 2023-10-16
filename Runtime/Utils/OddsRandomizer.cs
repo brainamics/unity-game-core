1334f0c33e1384da892427809b465e2ad13ea71f
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-10-16 15:45:06 +0200

    Update OddsRandomizer.cs

--- Runtime/Utils/OddsRandomizer.cs
+++ Runtime/Utils/OddsRandomizer.cs
@@ -6,13 +6,17 @@ namespace Brainamics.Core
 {
     public sealed class OddsRandomizer<T>
     {
-        private readonly List<Odds> _odds = new();
+        private readonly List<OddsRecord> _odds = new();
         private double _maxOdds;
-
+    
+        public int OddsCount => _odds.Count;
+    
+        public bool IsValid => _maxOdds > 0;
+    
         public void AddOdds(T value, double odds)
         {
             var end = _maxOdds + odds;
-            _odds.Add(new Odds
+            _odds.Add(new OddsRecord
             {
                 Value = value,
                 Begin = _maxOdds,
@@ -20,7 +24,7 @@ namespace Brainamics.Core
             });
             _maxOdds = end;
         }
-
+    
         public T Randomize()
         {
             var selected = Random.value * _maxOdds;
@@ -30,14 +34,19 @@ namespace Brainamics.Core
                     continue;
                 return odds.Value;
             }
-            throw new System.InvalidOperationException("Unexpected algorithm issue.");
+            throw new System.InvalidOperationException($"Unexpected algorithm issue. Odds record = {string.Join(',', _odds.Select(o => o.ToString()))}");
         }
-
-        private struct Odds
+    
+        private struct OddsRecord
         {
             public double Begin;
             public double End;
             public T Value;
+    
+            public override readonly string ToString()
+            {
+                return $"{Begin}-{End}=>{Value}";
+            }
         }
     }
 }
