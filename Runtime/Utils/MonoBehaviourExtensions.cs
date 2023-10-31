e96bcf7e59842419c94a8f00e9b6671c15004216
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-10-31 12:13:12 +0100

    Update MonoBehaviourExtensions.cs

--- Runtime/Utils/MonoBehaviourExtensions.cs
+++ Runtime/Utils/MonoBehaviourExtensions.cs
@@ -7,6 +7,18 @@ namespace Brainamics.Core
 {
     public static class MonoBehaviourExtensions
     {
+        public static void SkipFramesAndRun(this MonoBehaviour monoBehaviour, int frames, Action action)
+        {
+            monoBehaviour.StartCoroutine(RunCoroutine());
+
+            IEnumerator RunCoroutine()
+            {
+                for (; frames > 0; frames--)
+                    yield return null;
+                action();
+            }
+        }
+        
         public static void RunOnNextFrame(this MonoBehaviour monoBehaviour, Action action)
         {
             monoBehaviour.StartCoroutine(RunNextFrameCoroutine());
