f25db302e0f0bf177b53e74a6803e5f9576f3a23
Javid Sh <javidsoft@yahoo.com>
2023-11-08 16:25:26 +0100

    Update enum utils

---
+++ Runtime/EnumUtils/StringEnum.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public class StringEnum
+    {
+        private readonly Dictionary<string, int> _nameValueMap = new();
+        private readonly List<string> _valueNameMap = new();
+
+        public int Parse(string key)
+        {
+            if (_nameValueMap.TryGetValue(key, out var val))
+                return val;
+            return DefineNew(key);
+        }
+
+        public string GetName(int value)
+        {
+            if (value < 0 || value >= _valueNameMap.Count)
+                throw new System.ArgumentOutOfRangeException(nameof(value));
+
+            return _valueNameMap[value];
+        }
+
+        private int DefineNew(string name)
+        {
+            var val = _valueNameMap.Count;
+            _nameValueMap.Add(name, val);
+            _valueNameMap.Add(name);
+            return val;
+        }
+    }
+}
