f714c7431f8957967fea98931af71d2e7754c873
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-12-15 10:01:05 +0100

    Update IUIWindow.cs

--- Runtime/UI/IUIWindow.cs
+++ Runtime/UI/IUIWindow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Brainamics.Core
 {
@@ -11,5 +12,7 @@ namespace Brainamics.Core
         bool CanClose { get; }
 
         bool IsVisible { get; set; }
+
+        UnityEvent<IUIWindow> OnVisibilityChanged { get; }
     }
 }
