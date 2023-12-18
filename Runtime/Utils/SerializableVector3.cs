2305b6e9f2e3f3d6b8919ab114a8a800f92ae9f1
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2023-12-18 17:08:43 +0100

    Update SerializableVector3.cs

--- Runtime/Utils/SerializableVector3.cs
+++ Runtime/Utils/SerializableVector3.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct SerializableVector3
+namespace Brainamics.Core
 {
-    public SerializableVector3(float x, float y, float z)
+    public struct SerializableVector3
     {
-        X = x;
-        Y = y;
-        Z = z;
-    }
-
-    public SerializableVector3(Vector3 value)
-    {
-        X = value.x;
-        Y = value.y;
-        Z = value.z;
-    }
-
-    public float X { get; set; }
-
-    public float Y { get; set; }
-
-    public float Z { get; set; }
-
-    public static implicit operator Vector3(SerializableVector3 value)
-    {
-        return new(value.X, value.Y, value.Z);
-    }
-
-    public static implicit operator SerializableVector3(Vector3 value)
-    {
-        return new SerializableVector3(value);
+        public SerializableVector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+    
+        public SerializableVector3(Vector3 value)
+        {
+            X = value.x;
+            Y = value.y;
+            Z = value.z;
+        }
+    
+        public float X { get; set; }
+    
+        public float Y { get; set; }
+    
+        public float Z { get; set; }
+    
+        public static implicit operator Vector3(SerializableVector3 value)
+        {
+            return new(value.X, value.Y, value.Z);
+        }
+    
+        public static implicit operator SerializableVector3(Vector3 value)
+        {
+            return new SerializableVector3(value);
+        }
     }
 }
