d970a79e1464828fdc5fc77a0e73483354de8bc2
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2024-01-25 14:49:26 +0100

    Add files via upload

---
+++ Runtime/Data/CellAddress.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public readonly struct CellAddress
+{
+    public static readonly CellAddress Invalid = new(-1, -1);
+
+    public readonly int Row;
+    public readonly int Column;
+
+    public bool IsValid => Row >= 0 && Column >= 0;
+
+    public CellAddress(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public CellAddress MoveOneStep(PrimaryDirection direction)
+    {
+        return direction switch
+        {
+            PrimaryDirection.Up => new CellAddress(Row - 1, Column),
+            PrimaryDirection.Down => new CellAddress(Row + 1, Column),
+            PrimaryDirection.Left => new CellAddress(Row, Column - 1),
+            PrimaryDirection.Right => new CellAddress(Row, Column + 1),
+            _ => throw new NotImplementedException($"Moving one step towards {direction} is not implemented."),
+        };
+    }
+    
+    public CellAddress MoveStep(Vector2Int amount)
+    {
+        return new CellAddress(Row - amount.y, Column + amount.x);
+    }
+    
+    public bool IsOnTop()
+    {
+        return Row == 0;
+    }
+
+    public CellAddress GetNearestInvalidAddress(int rows, int columns)
+    {
+        var row = Row;
+        var column = Column;
+        if (!IsValid)
+            return this;
+
+        var distances = new ValueTuple<int, Action>[] {
+            (Row, () => row = -1),
+            (Column, () => column = -1),
+            (rows - Row, () => row = rows),
+            (columns - Column, () => column = columns),
+        };
+        var min = distances[0];
+        foreach (var distance in distances)
+            if (distance.Item1 < min.Item1)
+                min = distance;
+        min.Item2();
+
+        return new CellAddress(row, column);
+    }
+
+    public override string ToString()
+    {
+        return $"{Row},{Column}";
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is CellAddress addr)
+            return Row == addr.Row && Column == addr.Column;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Column);
+    }
+
+    public static bool operator ==(CellAddress a, CellAddress b)
+    {
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(CellAddress a, CellAddress b)
+    {
+        return !a.Equals(b);
+    }
+
+    public float DistanceToTop()
+    {
+        return Row;
+    }
+}
