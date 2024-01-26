8e995a65d284862b7fffa102c1a8393525c7ef4f
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2024-01-26 15:10:23 +0100

    Update PrimaryDirection.cs

--- Runtime/Data/PrimaryDirection.cs
+++ Runtime/Data/PrimaryDirection.cs
@@ -35,4 +35,40 @@ public static class PrimaryDirectionUtils
 
     public static PrimaryDirection Rotate180(this PrimaryDirection dir)
         => dir.Rotate90Clockwise(2);
+
+    public static PrimaryDirection GetSwipeDirection(Vector3 start, Vector3 end)
+    {
+        var angle = GetSwipeAngle(start, end);
+        return DirectionFromAngle(angle);
+    }
+
+    public static float GetSwipeAngle(Vector3 start, Vector3 end)
+    {
+        var diff = end - start;
+
+        var pivotVector = new Vector3(1f, 0f, 0f);
+        var angle = Vector3.Angle(pivotVector, diff);
+        if (diff.z < 0)
+            angle = -angle;
+        return angle;
+    }
+
+    private static PrimaryDirection DirectionFromAngle(float angle)
+    {
+        if (angle >= 360f)
+        {
+            angle -= Mathf.Floor(angle / 360f) * 360f;
+        }
+        while (angle < 0f)
+            angle += 360f;
+    
+        return angle switch
+        {
+            (>= 0f and < 45f) or (>= 315f) => PrimaryDirection.Right,
+            >= 45f and < 135f => PrimaryDirection.Up,
+            >= 135f and < 225f => PrimaryDirection.Left,
+            >= 225f and < 315f => PrimaryDirection.Down,
+            _ => PrimaryDirection.None,
+        };
+    }
 }
