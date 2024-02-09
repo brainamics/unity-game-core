499c67ce1d0721a64d26f684ef703348fe33bc42
brainiacmix <javid.shoaei@brainamics.de>
2024-02-09 11:30:35 +0100

    Add math utils

---
+++ Runtime/Utils/MathUtils.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public static class MathUtils
+    {
+        public static float Remap(float value, float from1, float to1, float from2, float to2)
+        {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+    }
+}
