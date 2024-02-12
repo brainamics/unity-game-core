3293fe60edcf265a400b6eefbb833593aa6ab415
brainiacmix <javid.shoaei@brainamics.de>
2024-02-12 17:34:02 +0100

    Add color extensions

---
+++ Runtime/Utils/ColorExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public static class ColorExtensions
+    {
+        public static Color WithAlpha(this Color color, float alpha)
+        {
+            return new Color(color.r, color.g, color.g, alpha);
+        }
+    }
+}
