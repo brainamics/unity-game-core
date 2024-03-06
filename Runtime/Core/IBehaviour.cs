63c0bf7cc4011c892f42aeae32d7fa0993ed9891
brainiacmix <javid.shoaei@brainamics.de>
2024-03-06 17:41:24 +0100

    add enabled to behavior

--- Runtime/Core/IBehaviour.cs
+++ Runtime/Core/IBehaviour.cs
@@ -7,6 +7,8 @@ namespace Brainamics.Core
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")]
     public interface IBehaviour
     {
+        bool enabled { get; set; }
+
         Transform transform { get; }
 
         GameObject gameObject { get; }
