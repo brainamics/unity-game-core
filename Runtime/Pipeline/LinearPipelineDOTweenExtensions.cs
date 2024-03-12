13235d61be178ca14c3c7a4ca2eb818cff7cc626
brainiacmix <javid.shoaei@brainamics.de>
2024-03-12 10:52:16 +0100

    update dotween

--- Runtime/Pipeline/LinearPipelineDOTweenExtensions.cs
+++ Runtime/Pipeline/LinearPipelineDOTweenExtensions.cs
@@ -1,15 +1,22 @@
-#define DOTWEEN
 #if DOTWEEN
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Brainamics.Core
 {
     public static class LinearPipelineDOTweenExtensions
     {
-        public static void RegisterFeedback()
+        public static void RegisterFeedback(this LinearPipeline pipeline, Tween tween)
         {
+            if (tween.IsComplete())
+                return;
+            pipeline.RegisterFeedbackHandle(tween);
+            tween.OnComplete(() =>
+            {
+                pipeline.UnregisterFeedbackHandle(tween);
+            });
         }
     }
 }
