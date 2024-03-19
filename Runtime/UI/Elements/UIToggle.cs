794b6226097ab58c6a03143382b6347ef0b7670e
brainiacmix <javid.shoaei@brainamics.de>
2024-03-19 17:45:21 +0100

    add ui toggle

---
+++ Runtime/UI/Elements/UIToggle.cs
@@ -0,0 +1,51 @@
+using Brainamics.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Brinamics.Core
+{
+    [RequireComponent(typeof(Toggle))]
+    public class UIToggle : MonoBehaviour
+    {
+        private Toggle _toggle;
+        private bool _checked;
+
+        public AnimationGroup OnFeedback;
+        public AnimationGroup OffFeedback;
+
+        private void Awake()
+        {
+            _toggle = GetComponent<Toggle>();
+        }
+
+        private void OnEnable()
+        {
+            _toggle.onValueChanged.AddListener(HandleValueChanged);
+            UpdateState();
+        }
+
+        private void OnDisable()
+        {
+            _toggle.onValueChanged.RemoveListener(HandleValueChanged);
+        }
+
+        private void HandleValueChanged(bool _)
+        {
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            if (_checked == _toggle.isOn)
+                return;
+            _checked = _toggle.isOn;
+
+            var enabled = _checked ? OnFeedback : OffFeedback;
+            var disabled = _checked ? OffFeedback : OnFeedback;
+            if (disabled != null)
+                disabled.Kill();
+            if (enabled != null)
+                enabled.Play();
+        }
+    }
+}
