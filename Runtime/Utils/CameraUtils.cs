8151331d6157ce8dc0fc4f84b42bef293bab3523
brainiacmix <javid.shoaei@brainamics.de>
2024-03-20 16:55:10 +0100

    update camera

--- Runtime/Utils/CameraUtils.cs
+++ Runtime/Utils/CameraUtils.cs
@@ -6,20 +6,31 @@ namespace Brainamics.Core
 {
     public static class CameraUtils
     {
-        public static void FitToBounds(Camera camera, Bounds bounds)
+        public static void FitToBounds(Camera camera, Bounds bounds, bool adjustPosition = true)
         {
-            var screenAspect = (float)Screen.width / (float)Screen.height;
+            var screenAspect = Screen.width / Screen.height;
             var boundsAspect = bounds.size.x / bounds.size.y;
 
             var orthographicSize = bounds.size.y / 2;
 
-            if (boundsAspect > screenAspect)
+            //if (boundsAspect > screenAspect)
+            //{
+            //    orthographicSize = bounds.size.x / (2 * screenAspect);
+            //}
+
+            if (screenAspect >= boundsAspect)
             {
-                orthographicSize = bounds.size.x / (2 * screenAspect);
+                orthographicSize = bounds.size.y / 2;
+            }
+            else
+            {
+                var differenceInSize = boundsAspect / screenAspect;
+                orthographicSize = bounds.size.y / 2 * differenceInSize;
             }
 
             camera.orthographicSize = orthographicSize;
-            camera.transform.position = new Vector3(bounds.center.x, camera.transform.position.y, bounds.center.z);
+            if (adjustPosition)
+                camera.transform.position = new Vector3(bounds.center.x, camera.transform.position.y, bounds.center.z);
         }
     }
 }
