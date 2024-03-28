899e56c1d0d4c766485bf00de62347f7bb4203c7
brainiacmix <javid.shoaei@brainamics.de>
2024-03-28 17:44:11 +0100

    update factory

--- Runtime/Factory/GameObjectFactory.cs
+++ Runtime/Factory/GameObjectFactory.cs
@@ -12,6 +12,18 @@ namespace Brainamics.Core
         [SerializeField]
         private GameObject _prefab;
 
+        public Transform Parent
+        {
+            get => _parent;
+            set => _parent = value;
+        }
+
+        public GameObject Prefab
+        {
+            get => _prefab;
+            set => _prefab = value;
+        }
+
         public override GameObject Create()
         {
             return Instantiate(_prefab, _parent);
