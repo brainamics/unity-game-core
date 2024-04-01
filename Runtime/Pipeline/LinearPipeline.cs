43baccf11bb12565e0757d3f115aae11b5cd9600
Javid Sh <javidsoft@yahoo.com>
2024-04-01 18:11:10 +0200

    fix pipeline

--- Runtime/Pipeline/LinearPipeline.cs
+++ Runtime/Pipeline/LinearPipeline.cs
@@ -6,98 +6,108 @@ using UnityEngine;
 
 namespace Brainamics.Core
 {
-    public class LinearPipeline
-    {
-        private readonly PriorityQueue<PipelineAction, PipelinePriority> _actionQueue = new();
-        private readonly HashSet<object> _feedbacks = new();
-        private readonly HashSet<System.Action> _processors = new();
-        private int _queueIndex = int.MinValue;
+	public class LinearPipeline
+	{
+		private readonly PriorityQueue<PipelineAction, PipelinePriority> _actionQueue = new();
+		private readonly HashSet<object> _feedbacks = new();
+		private readonly HashSet<System.Action> _processors = new();
+		private int _queueIndex = int.MinValue;
 
-        public bool AnyOngoingFeedbacks => _feedbacks.Count > 0;
+		public bool AnyOngoingFeedbacks => _feedbacks.Count > 0;
 
-        public void Clear()
-        {
-            _actionQueue.Clear();
-            _feedbacks.Clear();
-        }
+		public void Clear()
+		{
+			while (_actionQueue.TryDequeue(out var a, out _))
+			{
+				try
+				{
+					a.Action.Invoke();
+				}
+				catch (Exception exception)
+				{
+					Debug.LogError(exception);
+				}
+			}
+			_feedbacks.Clear();
+		}
 
-        public void EnqueueAction(PipelineAction action, int priority = 0)
-        {
-            // TODO check ID for duplicates
-            var pipelinePriority = CreatePriority(priority);
-            _actionQueue.Enqueue(action, pipelinePriority);
-        }
+		public void EnqueueAction(PipelineAction action, int priority = 0)
+		{
+			// TODO check ID for duplicates
+			var pipelinePriority = CreatePriority(priority);
+			_actionQueue.Enqueue(action, pipelinePriority);
+		}
 
-        public void EnqueueAction(object id, System.Action action, int priority= 0)
-            => EnqueueAction(new PipelineAction(id, action), priority);
+		public void EnqueueAction(object id, System.Action action, int priority = 0)
+			=> EnqueueAction(new PipelineAction(id, action), priority);
 
-        public void EnqueueAction(System.Action action, int priority= 0)
-            => EnqueueAction(new PipelineAction(action), priority);
+		public void EnqueueAction(System.Action action, int priority = 0)
+			=> EnqueueAction(new PipelineAction(action), priority);
 
-        public void RegisterProcessor(System.Action processor)
-            => _processors.Add(processor);
+		public void RegisterProcessor(System.Action processor)
+			=> _processors.Add(processor);
 
-        public void UnregisterProcessor(System.Action processor)
-            => _processors.Remove(processor);
+		public void UnregisterProcessor(System.Action processor)
+			=> _processors.Remove(processor);
 
-        public void RegisterFeedback(Task task)
-        {
-            RegisterFeedbackHandle(task);
-            task.ContinueWith(UnregisterFeedbackHandle);
-        }
+		public void RegisterFeedback(Task task)
+		{
+			RegisterFeedbackHandle(task);
+			task.ContinueWith(UnregisterFeedbackHandle);
+		}
 
-        public IDisposable RegisterFeedbackHandle()
-        {
-            var handle = new object();
-            RegisterFeedbackHandle(handle);
-            return new CallbackDisposable(() => UnregisterFeedbackHandle(handle));
-        }
+		public IDisposable RegisterFeedbackHandle()
+		{
+			var handle = new object();
+			RegisterFeedbackHandle(handle);
+			return new CallbackDisposable(() => UnregisterFeedbackHandle(handle));
+		}
 
-        public void RegisterFeedbackHandle(object feedback)
-        {
-            if (!_feedbacks.Add(feedback))
-                throw new System.InvalidOperationException("Could not register feedback; it's already registered.");
-        }
+		public void RegisterFeedbackHandle(object feedback)
+		{
+			if (!_feedbacks.Add(feedback))
+				throw new System.InvalidOperationException("Could not register feedback; it's already registered.");
+		}
 
-        public bool UnregisterFeedbackHandle(object feedback)
-        {
-            return _feedbacks.Remove(feedback);
-        }
+		public bool UnregisterFeedbackHandle(object feedback)
+		{
+			return _feedbacks.Remove(feedback);
+		}
 
-        public bool TriggerProcess()
-        {
-            foreach (var processor in _processors)
-                processor.Invoke();
+		public bool TriggerProcess()
+		{
+			foreach (var processor in _processors)
+				processor.Invoke();
 
-            var processed = false;
+			var processed = false;
 
-            while (true)
-            {
-                if (AnyOngoingFeedbacks)
-                    return processed;
+			while (true)
+			{
+				if (AnyOngoingFeedbacks)
+					return processed;
 
-                var result = ExecuteAction();
-                if (!result)
-                    return processed;
-                processed = true;
-            }
-        }
+				var result = ExecuteAction();
+				if (!result)
+					return processed;
+				processed = true;
+			}
+		}
 
-        public bool ExecuteAction()
-        {
-            if (!_actionQueue.TryDequeue(out var action, out _))
-                return false;
+		public bool ExecuteAction()
+		{
+			if (!_actionQueue.TryDequeue(out var action, out _))
+				return false;
 
-            action.Action.Invoke();
-            return true;
-        }
+			action.Action.Invoke();
+			return true;
+		}
 
-        private PipelinePriority CreatePriority(int priority = 0)
-        {
-            unchecked
-            {
-                return new PipelinePriority(priority, _queueIndex++);
-            }
-        }
-    }
+		private PipelinePriority CreatePriority(int priority = 0)
+		{
+			unchecked
+			{
+				return new PipelinePriority(priority, _queueIndex++);
+			}
+		}
+	}
 }
