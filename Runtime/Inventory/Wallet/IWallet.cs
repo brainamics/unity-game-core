2297d0ccb0b15a87c529a13e2186a3159eb9c54c
brainiacmix <javid.shoaei@brainamics.de>
2024-04-12 09:07:19 +0200

    add wallet

---
+++ Runtime/Inventory/Wallet/IWallet.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Brainamics.Core
+{
+    public interface IWallet<TCurrency> : IEnumerable<ValueTuple<TCurrency, BigInteger>>
+    {
+        event WalletCurrencyChangeHandler<TCurrency> OnCurrencyChanged;
+
+        /// <summary>
+        /// Gets or sets the balance of the specified currency.
+        /// </summary>
+        BigInteger this[TCurrency currency] { get; set; }
+
+        BigInteger GetBalance(TCurrency currency);
+
+        void SetBalance(TCurrency currency, BigInteger value);
+
+        BigInteger AddBalance(TCurrency currency, BigInteger amount);
+
+        bool TryUseBalance(TCurrency currency, BigInteger amount);
+
+        void UseBalance(TCurrency currency, BigInteger amount);
+    }
+}
---
+++ Runtime/Inventory/Wallet/Wallet.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Brainamics.Core
+{
+    public class Wallet<TCurrency> : IWallet<TCurrency>
+    {
+        private readonly HybridDictionary _infoMap = new();
+
+        public event WalletCurrencyChangeHandler<TCurrency> OnCurrencyChanged;
+
+        public BigInteger this[TCurrency currency]
+        {
+            get => GetBalance(currency);
+            set => SetBalance(currency, value);
+        }
+
+        public BigInteger GetBalance(TCurrency currency)
+            => GetInfo(currency).Balance;
+
+        public bool CanAfford(TCurrency currency, BigInteger cost)
+            => GetBalance(currency) >= cost;
+
+        public void SetBalance(TCurrency currency, BigInteger value)
+        {
+            if (value < 0)
+                throw new System.ArgumentOutOfRangeException(nameof(value));
+            var info = GetInfo(currency);
+            var oldValue = info.Balance;
+            if (oldValue == value)
+                return;
+            info.Balance = value;
+            OnCurrencyChanged?.Invoke(this, currency, oldValue, value);
+        }
+
+        public BigInteger AddBalance(TCurrency currency, BigInteger amount)
+        {
+            var result = this[currency] + amount;
+            SetBalance(currency, result);
+            return result;
+        }
+
+        public bool TryUseBalance(TCurrency currency, BigInteger amount)
+        {
+            var remaining = this[currency];
+            if (remaining < amount)
+                return false;
+            this[currency] -= amount;
+            return true;
+        }
+
+        public void UseBalance(TCurrency currency, BigInteger amount)
+        {
+            if (TryUseBalance(currency, amount))
+                return;
+            throw new System.InvalidOperationException($"Insufficient {currency} balance.");
+        }
+
+        public IEnumerator<(TCurrency, BigInteger)> GetEnumerator()
+        {
+            foreach (var currency in _infoMap)
+            {
+                var info = (CurrencyInfo)_infoMap[currency];
+                yield return (info.Currency, info.Balance);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private CurrencyInfo GetInfo(TCurrency currency)
+            => (CurrencyInfo)(_infoMap[currency] ??= new CurrencyInfo(currency));
+
+        private sealed class CurrencyInfo
+        {
+            public TCurrency Currency;
+            public BigInteger Balance;
+
+            public CurrencyInfo(TCurrency currency)
+            {
+                Currency = currency;
+            }
+        }
+    }
+}
