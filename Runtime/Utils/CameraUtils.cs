32748f882c5868fe997a35ffd553f06f9e575733
brainiacmix <javid.shoaei@brainamics.de>
2024-04-30 13:41:21 +0200

    add x cam adjuster

--- Runtime/Utils/CameraUtils.cs
+++ Runtime/Utils/CameraUtils.cs
@@ -22,5 +22,22 @@ namespace Brainamics.Core
             if (adjustPosition)
                 camera.transform.position = new Vector3(bounds.center.x, camera.transform.position.y, bounds.center.z);
         }
+
+        public static void FitToBoundsX(Camera camera, Bounds bounds, bool adjustPosition = true)
+        {
+            var screenAspect = (float)Screen.width / Screen.height;
+            var boundsAspect = bounds.size.z / bounds.size.y;
+
+            var orthographicSize = bounds.size.y / 2;
+
+            if (boundsAspect > screenAspect)
+            {
+                orthographicSize = bounds.size.z / (2 * screenAspect);
+            }
+
+            camera.orthographicSize = orthographicSize;
+            if (adjustPosition)
+                camera.transform.position = new Vector3(bounds.center.x, camera.transform.position.y, bounds.center.z);
+        }
     }
 }
