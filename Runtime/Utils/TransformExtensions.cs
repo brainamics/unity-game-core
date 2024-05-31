2e5e54f77a293cab12fbe5bfcd4ea6a55bc151bf
brainiacmix <javid.shoaei@brainamics.de>
2024-05-31 14:24:43 +0200

    update transform ext

--- Runtime/Utils/TransformExtensions.cs
+++ Runtime/Utils/TransformExtensions.cs
@@ -9,29 +9,41 @@ namespace Brainamics.Core
 {
     public static class TransformExtensions
     {
-	public static IEnumerable<Transform> EnumerateObjects(this Scene scene)
-	{
-		return scene.GetRootGameObjects().SelectMany(o => o.transform.EnumerateChildren());
-	}
-	    
+        public static IEnumerable<Transform> EnumerateObjects(this Scene scene)
+        {
+            return scene.GetRootGameObjects().SelectMany(o => o.transform.EnumerateDescendants());
+        }
+
         public static IEnumerable<Transform> EnumerateChildren(this Transform transform)
         {
             for (var i = 0; i < transform.childCount; i++)
                 yield return transform.GetChild(i);
         }
 
+        public static IEnumerable<Transform> EnumerateDescendants(this Transform transform, bool includeSelf = true)
+        {
+            if (includeSelf)
+                yield return transform;
+
+            foreach (var child in transform.EnumerateChildren())
+            {
+                foreach (var grandChild in child.EnumerateDescendants())
+                    yield return grandChild;
+            }
+        }
+
         public static Rect GetWorldRect(this RectTransform rt, Vector2 scale)
         {
-    		// Convert the rectangle to world corners and grab the top left
-    		Vector3[] corners = new Vector3[4];
-    		rt.GetWorldCorners(corners);
-    		Vector3 topLeft = corners[0];
-    
-    		// Rescale the size appropriately based on the current Canvas scale
-    		Vector2 scaledSize = new Vector2(scale.x * rt.rect.size.x, scale.y * rt.rect.size.y);
-    
-    		return new Rect(topLeft, scaledSize);
-	    }
+            // Convert the rectangle to world corners and grab the top left
+            Vector3[] corners = new Vector3[4];
+            rt.GetWorldCorners(corners);
+            Vector3 topLeft = corners[0];
+
+            // Rescale the size appropriately based on the current Canvas scale
+            var scaledSize = new Vector2(scale.x * rt.rect.size.x, scale.y * rt.rect.size.y);
+
+            return new Rect(topLeft, scaledSize);
+        }
 
         public static Rect GetWorldRect(this RectTransform rt, Canvas canvas)
         {
