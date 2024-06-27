c6ca7b7df36d43619fabc0e3f6325d24592add71
javid.shoaei <javid.shoaei@brainamics.de>
2024-06-27 11:34:32 +0200

    update player prefs

---
+++ Runtime/Persistence/PlayerPrefsPersistenceProvider.cs
@@ -0,0 +1,66 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    //[CreateAssetMenu(menuName = "Game/Services/Persistence/PlayerPrefs Persistence Provider")]
+    //public class PlayerPrefsPersistenceProvider : PlayerPrefsPersistenceProvider<GameStateData> { }
+
+    public class PlayerPrefsPersistenceProvider<TState> : PersistenceProviderBase<TState>
+    {
+        [SerializeField]
+        private PersistenceServiceBase<TState> _persistenceService;
+
+
+        [SerializeField]
+        private string _key = "SAVEDATA";
+
+        public override Task<TCustomState> LoadStateAsync<TCustomState>()
+        {
+            return Task.FromResult(LoadStateFromPrefs<TCustomState>());
+        }
+
+        public override Task<TState> LoadStateAsync()
+        {
+            return Task.FromResult(LoadStateFromPrefs());
+        }
+
+        public override Task SaveStateAsync(TState state)
+        {
+            SaveStateToPrefs(state);
+            return Task.CompletedTask;
+        }
+
+        private TCustomState LoadStateFromPrefs<TCustomState>()
+        {
+            try
+            {
+                var data = PlayerPrefs.GetString(_key);
+                if (string.IsNullOrEmpty(data))
+                {
+                    return default;
+                }
+
+                return JsonConvert.DeserializeObject<TCustomState>(data);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private TState LoadStateFromPrefs()
+        {
+            return LoadStateFromPrefs<TState>();
+        }
+
+        private void SaveStateToPrefs(TState state)
+        {
+            PlayerPrefs.SetString(_key, JsonConvert.SerializeObject(state));
+        }
+    }
+}
