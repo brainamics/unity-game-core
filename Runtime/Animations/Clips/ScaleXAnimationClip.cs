f8fcec19f7e529fcd1bbff12f6635f6af68aa69f
Philipp Zent <javid.shoaei@brainamics.de>
2024-11-18 14:49:48 +0100

    Refactor animation clips

---
+++ Runtime/Animations/Clips/ScaleXAnimationClip.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    [System.Serializable]
+    [AnimationClip(MenuName = "Transform/Scale X")]
+    public class ScaleXAnimationClip : TransformAnimationClipBase
+    {
+        public bool AutoFromScale;
+        public float FromScale;
+        public float ToScale = 1;
+
+        protected override IEnumerator PlayCoroutine(MonoBehaviour behaviour)
+        {
+            var transform = GetTargetTransform(behaviour);
+            var fromScale = AutoFromScale ? transform.localScale.x : FromScale;
+            return RunTimedLoop(lerp =>
+            {
+                var scale = transform.localScale;
+                scale.x = Mathf.LerpUnclamped(fromScale, ToScale, lerp);
+                transform.localScale = scale;
+            });
+        }
+    }
+}
