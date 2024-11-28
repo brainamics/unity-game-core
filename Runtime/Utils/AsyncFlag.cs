7d6eca878d8665cfa16d1cbc87ecb1b97dc70f49
javidsho <javidsoft@yahoo.com>
2024-11-28 11:43:49 +0100

    add AsyncFlag+BusyFlag

---
+++ Runtime/Utils/AsyncFlag.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    public sealed class AsyncFlag
+    {
+        private bool _value;
+
+        public event System.Action<AsyncFlag> OnValueChanged;
+
+        public bool IsSet
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                    return;
+                _value = value;
+                OnValueChanged?.Invoke(this);
+            }
+        }
+
+        public static UniTask WaitForAll(IEnumerable<AsyncFlag> flags, bool value)
+        {
+            Func<AsyncFlag, UniTask> taskSelector;
+            if (value)
+                taskSelector = flag => flag.WaitForAsync(true);
+            else
+                taskSelector = flag => flag.WaitForAsync(false);
+            return UniTask.WhenAll(flags.Select(taskSelector));
+        }
+
+        public async UniTask WaitForAsync(bool value)
+        {
+            while (true)
+            {
+                if (IsSet == value)
+                    return;
+
+                await WaitForNextValue();
+            }
+        }
+
+        private UniTask WaitForNextValue()
+        {
+            var taskSource = AutoResetUniTaskCompletionSource.Create();
+            OnValueChanged += HandleValueChanged;
+            return taskSource.Task;
+
+            void HandleValueChanged(AsyncFlag self)
+            {
+                OnValueChanged -= HandleValueChanged;
+                taskSource.TrySetResult();
+            }
+        }
+    }
+}
---
+++ Runtime/Utils/BusyFlag.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+#if PRIMETWEEN
+using PrimeTween;
+#endif
+
+namespace Brainamics.Core
+{
+    public sealed class BusyFlag : AsyncCounterFlag
+    {
+        private readonly Action _decrementCounter;
+
+        public BusyFlag()
+        {
+            _decrementCounter = DecrementCounter;
+        }
+
+        public static UniTask Wait(IEnumerable<AsyncCounterFlag> flags)
+        {
+            return WaitForAll(flags, false);
+        }
+
+        public UniTask WaitForAsync()
+            => WaitForAsync(false);
+
+        public System.IDisposable SetBusy()
+        {
+            Counter++;
+            return new CallbackDisposable(_decrementCounter);
+        }
+
+        #if PRIMETWEEN
+        public void SetBusy(float duration)
+        {
+            if (duration <= 0)
+                return;
+
+            Counter++;
+            Tween.Delay(duration, DecrementCounter);
+        }
+        #endif
+
+        private void DecrementCounter()
+        {
+            Counter--;
+        }
+    }
+}
