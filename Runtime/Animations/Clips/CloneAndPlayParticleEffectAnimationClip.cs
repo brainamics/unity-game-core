11100e21072382540c4167c2275b435357721c5c
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-01-21 17:15:00 +0100

    Add files via upload

---
+++ Runtime/Animations/Clips/CloneAndPlayParticleEffectAnimationClip.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Brainamics.Core;
+using UnityEngine;
+
+[System.Serializable]
+[AnimationClip(MenuName = "Particle System/Clone and Play")]
+public class CloneAndPlayParticleEffectAnimationClip : AnimationClipBase
+{
+    private LevelObject _levelObject;
+
+    public ParticleSystem ParticleSystem;
+    public bool ReparentToLevel = true;
+    public bool PlayParticleSystem;
+
+    protected override IEnumerator PlayCoroutine(MonoBehaviour behaviour)
+    {
+        var scanner = GameSceneScanner.LocateByActiveScene();
+        var levelScanner = scanner.Level;
+
+        var parent = ParticleSystem.transform.parent;
+        if (ReparentToLevel)
+            parent = levelScanner.LevelObject.transform;
+        var orgTransform = ParticleSystem.transform;
+        var particleSystem = Object.Instantiate(ParticleSystem, orgTransform.position, orgTransform.rotation, parent);
+        particleSystem.gameObject.SetActive(true);
+
+        if (PlayParticleSystem)
+            particleSystem.Play();
+        yield break;
+    }
+}
