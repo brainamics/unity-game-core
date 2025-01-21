231a2392c1fbc58d34b4df2892bf0f06d8f92a0e
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-01-21 14:27:22 +0100

    Add files via upload

---
+++ Runtime/Animations/Clips/SizeDeltaAnimationClip.cs
@@ -0,0 +1,26 @@
+﻿using Brainamics.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    [System.Serializable]
+    [AnimationClip(MenuName = "Transform/Size Delta")]
+    public class SizeDeltaAnimationClip : TransformAnimationClipBase
+    {
+        public bool AutoFromSize;
+        public bool AutoToSize;
+        public Vector2 FromSize;
+        public Vector2 ToSize;
+
+        protected override IEnumerator PlayCoroutine(MonoBehaviour behaviour)
+        {
+            var transform = (RectTransform)GetTargetTransform(behaviour);
+            var fromSize = AutoFromSize ? transform.anchoredPosition : FromSize;
+            var toSize = AutoToSize ? transform.anchoredPosition : ToSize;
+            return RunTimedLoop(lerp =>
+                transform.sizeDelta = Vector2.LerpUnclamped(fromSize, toSize, lerp));
+        }
+    }
+}
