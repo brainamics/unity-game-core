444ce978ba8caac3389cfaeb03f05ae2c0328091
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-01-21 14:34:08 +0100

    Add files via upload

---
+++ Runtime/Animations/Clips/ImageSpriteAnimationClip.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Brainamics.Core
+{
+    [System.Serializable]
+    [AnimationClip(MenuName = "Canvas/Image Sprite")]
+    public class ImageSpriteAnimationClip : AnimationClipBase
+    {
+        public Image Image;
+        public Sprite Sprite;
+
+        public override void PlayImmediate(MonoBehaviour behaviour)
+        {
+            ApplySprite();
+        }
+
+        protected override IEnumerator PlayCoroutine(MonoBehaviour behaviour)
+        {
+            ApplySprite();
+            yield break;
+        }
+
+        private void ApplySprite()
+        {
+            Image.sprite = Sprite;
+        }
+    }
+}
