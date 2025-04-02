f76c15a78d8bc93e2d73b0b24032afa3758ace04
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-04-02 15:39:28 +0200

    Update VectorExtensions.cs

--- Runtime/Utils/VectorExtensions.cs
+++ Runtime/Utils/VectorExtensions.cs
@@ -1,23 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace Brainamics.Core
 {
     public static class VectorExtensions
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithX(this Vector3 vector, float x)
             => new(x, vector.y, vector.z);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithY(this Vector3 vector, float y)
             => new(vector.x, y, vector.z);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithZ(this Vector3 vector, float z)
             => new(vector.x, vector.y, z);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 WithX(this Vector2 vector, float x)
             => new(x, vector.y);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 WithY(this Vector2 vector, float y)
             => new(vector.x, y);
 
