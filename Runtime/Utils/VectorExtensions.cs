ca519cdaceb2068d6a2f6896ddbad47df708f8db
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-04-02 15:38:20 +0200

    Update VectorExtensions.cs

--- Runtime/Utils/VectorExtensions.cs
+++ Runtime/Utils/VectorExtensions.cs
@@ -20,5 +20,15 @@ namespace Brainamics.Core
 
         public static Vector2 WithY(this Vector2 vector, float y)
             => new(vector.x, y);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector3 Lerp(Vector3 a, Vector3 b, Vector3 t)
+        {
+            return new Vector3(
+                Mathf.Lerp(a.x, b.x, t.x),
+                Mathf.Lerp(a.y, b.y, t.y),
+                Mathf.Lerp(a.z, b.z, t.z)
+            );
+        }
     }
 }
