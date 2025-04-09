d744226b2eade360e107a0c26d92586fc6d0fe7d
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-04-09 10:07:41 +0200

    Add files via upload

---
+++ Runtime/Animations/Clips/SetAnimatorStateAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Brainamics.Core
+{
+    [System.Serializable]
+    [AnimationClip(MenuName = "Particle System/Clone and Play")]
+    public class SetAnimatorStateAnimation : AnimationClipBase
+    {
+        public Animator Animator;
+        public string StateName;
+
+        [Min(0)]
+        public int Layer;
+
+        [Range(0, 1)]
+        public float NormalizedTime;
+
+        [Min(0)]
+        public float TransitionDuration;
+
+        protected override IEnumerator PlayCoroutine(MonoBehaviour behaviour)
+        {
+            if (TransitionDuration > 0)
+            {
+                Animator.CrossFade(StateName, TransitionDuration, Layer, NormalizedTime);
+                yield break;
+            }
+
+            Animator.Play(StateName, Layer, NormalizedTime);
+        }
+    }
+}
