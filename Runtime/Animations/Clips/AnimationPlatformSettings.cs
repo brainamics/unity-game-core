18c9183879a611fc4128045731ede2ad83282e31
brainiacmix <118723840+brainiacmix@users.noreply.github.com>
2025-04-15 10:13:19 +0200

    Update AnimationPlatformSettings.cs

--- Runtime/Animations/Clips/AnimationPlatformSettings.cs
+++ Runtime/Animations/Clips/AnimationPlatformSettings.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine;
 namespace Brainamics.Core
 {
     [System.Serializable]
-    public sealed class AnimationPlatformSettings
+    public sealed class AnimationPlatformSettings : IEquatable<AnimationPlatformSettings>, ISerializationCallbackReceiver
     {
         public bool AnyPlatform = true;
 
@@ -27,6 +28,38 @@ namespace Brainamics.Core
                     _ => true,
                 };
             }
+        }
+        
+        public bool Equals(AnimationPlatformSettings other)
+        {
+            if (other == null)
+                return false;
+
+            return AnyPlatform == other.AnyPlatform && Android == other.Android && IOS == other.IOS;
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if (obj is AnimationPlatformSettings other)
+                return Equals(other);
+            return false;
+        }
+        
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(AnyPlatform, Android, IOS);
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            if (AnyPlatform || Android || IOS)
+                return;
+
+            AnyPlatform = true;
         }
     }
 }
